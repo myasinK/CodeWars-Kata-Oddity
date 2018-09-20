e6c1c3a290b22cb50300aebf859f6cd523db73de
myasinK <yasinul@gmail.com>
2018-09-19 17:32:33 -0700

    Find int that occurs odd number of times

---
+++ Kata/Kata.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+namespace Kata
+{
+    public static class Kata
+    {
+        public static int find_it(int[] seq)
+        {
+            var freq = -1;
+            var it = -1;
+
+            foreach (var item in seq.ToList())
+            {
+                var numOccurence = seq.ToList().FindAll(x => x == item).Count;
+                if (numOccurence % 2 != 0 && numOccurence > freq)
+                {
+                    it = item;
+                    freq = numOccurence;
+                }
+            }
+            return it;
+        }
+    }
+}
---
+++ Kata/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    //Problem statement:
+    //Given an array, find the int that appears an odd number of times.
+
+    //There will always be only one integer that appears an odd number of times.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new[]{0,0,1,1,1,5,5,5,5,5}; 
+            
+            Console.WriteLine(Kata.find_it(array));
+        }
+    }
+}
